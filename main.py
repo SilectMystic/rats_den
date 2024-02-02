44ad098e53376800adbdc3167df62cf8ed4e9991
SilectMystic <cvasquez@brooklynsteamcenter.org>
2024-02-02 10:57:06 -0500

    more work

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 connection = pymysql.connect(
     database = 'cvasquez_rd',
     user = 'cvasquez',
-    password = contra,
+    password = f'{contra}',
     host = '10.100.33.60',
     cursorclass = pymysql.cursors.DictCursor
 )
@@ -17,12 +17,14 @@ connection = pymysql.connect(
 def index():
     return render_template('landing.html.jinja')
 
-@app.route('/sign_up', methods=['POSTS'])
+@app.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
+        new_username = request.form['new_username']
+        new_email = request.form['new_email']
+        new_password = request.form['new_password']
         cursor = connection.cursor()
-        new_username = request.form[new_username]
-        new_email = request.form[new_email]
-        new_password = request.form[new_password]
         cursor.execute(f'INSERT INTO `users` (`username`, `email`, `password`) VALUES ("{new_username}", "{new_email}", "{new_password}");')
+        cursor.close()
+        connection.commit()
     return render_template('sign_up.html')
